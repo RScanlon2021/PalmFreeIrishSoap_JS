b64800b0f381127da72fb8552c0bae47cd108a0a
RScanlon2021 <roryscanlon21@protonmail.com>
2022-08-11 13:33:37 +0100

    added google maps API displayed through a modal on contact page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ const header = document.querySelector("header");
 const navItems = document.querySelectorAll(".nav-link");
 const navLogo = document.querySelector(".nav-logo");
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnShowModal = document.querySelector(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+
+// const map = document.querySelector("#map");
+
 const stickyNav = function (entries) {
   const [entry] = entries;
   if (!entry.isIntersecting) {
@@ -64,10 +71,51 @@ function removeActiveClass() {
 
 ////////////////////////////////////////////////////////////////////////////////CONTACT PAGE
 
-// console.log(window.innerHeight);
+async function initMap() {
+  // The location of Killaloe
+  const killaloe = { lat: 52.80883432484582, lng: -8.44750810395629 };
+  // The map, centered at Killaloe
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: killaloe,
+  });
+  // The marker, positioned at Killaloe
+  const marker = new google.maps.Marker({
+    position: killaloe,
+    map: map,
+  });
+}
 
-// if (window.innerHeight < 600) {
-//   console.log("less than 600");
-// } else {
-//   console.log("more than 600");
-// }
+// const promise = new Promise((resolve, reject) => {
+//   const error = false;
+//   if (!error) {
+//     resolve("Promise has been fullfilled");
+//   } else {
+//     reject("Error: Operation has failed");
+//   }
+// });
+
+// console.log(promise);
+// promise.then((data) => console.log(data)).catch((err) => console.log(err));
+
+///////////////////////////////////////////////////////////////////////////////GOOGLE MAPS MODAL
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+////////////////////////////AOPEN AND CLOSE MODAL EVENT LISTENERS
+
+btnShowModal.addEventListener("click", showModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+document.addEventListener("keyup", (e) => {
+  if (e.key == "Escape") {
+    closeModal();
+  }
+});
